6463f69d512be88cafdd9b282a72c6cac4bbf3f7
m3m3ng01 <antarsayuridn@gmail.com>
2021-11-26 20:59:57 +0700

    first commit

---
+++ palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("masukkan kata yang ingin anda check: ");
+            string kata = Console.ReadLine();
+            char[] nameArray = kata.ToCharArray();
+            Array.Reverse(nameArray);
+            string kebalikan = new string(nameArray);
+            if (kata.Equals(kebalikan, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"{kata} adalah Palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{kata} bukan Palindrome");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
